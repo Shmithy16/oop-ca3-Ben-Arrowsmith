4a31f957e0b5eada154c7f5b9119e3e16569ca86
Shmithy16 <d00257746@student.dkit.ie>
2024-02-09 16:01:11 +0000

    Q6 finished

--- CA3_Starter_Code/src/CA3_Question6.java
+++ CA3_Starter_Code/src/CA3_Question6.java
@@ -11,6 +11,19 @@ import java.util.Deque;
 class Block{
     int qty;
     int price;
+
+    public Block(int qty, int price) {
+        this.qty = qty;
+        this.price = price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
 public class CA3_Question6
 {
@@ -34,15 +47,32 @@ public class CA3_Question6
             if(command.equalsIgnoreCase("buy"))
             {
                 int qty = in.nextInt();
-                double price = in.nextDouble();
-                totalValue.add(qty*price);
+                int price = in.nextInt();
+                blocks.add(new Block(qty,price));
             }
             else if(command.equals("sell"))
             {
-                int qty = in.nextInt();
-                double price = in.nextDouble();
-                double gain = price*qty-totalValue.remove();
-                System.out.print("The gain will be: " + gain);
+                int sellQty = in.nextInt();
+                int sellPrice = in.nextInt();
+                int totalGain = 0;
+                while(sellQty > 0 && !blocks.isEmpty()){
+                    Block buyQty = blocks.peek();
+                    if(sellQty >= buyQty.getQty()){
+                        sellQty = sellQty - buyQty.getQty();
+                        int sellCurrentPrice = sellPrice - buyQty.getPrice();
+                        int gain = buyQty.getQty()*sellCurrentPrice;
+                        System.out.println(gain);
+                        blocks.poll();
+                        totalGain += gain;
+                    }else if(sellQty < buyQty.getQty()){;
+                        sellQty = buyQty.getQty() - sellQty;
+                        int sellCurrentPrice = sellPrice - buyQty.getPrice();
+                        int gain = sellQty*sellCurrentPrice;
+                        System.out.println(gain);
+                        totalGain += gain;
+                    }
+                }
+                System.out.print("The gain will be: " + totalGain);
             }
         }while(!command.equalsIgnoreCase("quit"));
     }
